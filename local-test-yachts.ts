e373602536e3787231cf5384f33035285d936fa9
root <root@Rosa-Beams.localdomain>
2026-02-28 14:29:04 +0100

    debug: add debug query param to /api/yachts; remove separate debug routes

--- local-test-yachts.ts
+++ local-test-yachts.ts
@@ -1,7 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { eq, count } from 'drizzle-orm';
-import { yachtModels, manufacturers } from './drizzle/schema';
+import { yachtModels, manufacturers } from './drizzle/schema.ts';
 
 const connectionString = 'postgresql://neondb_owner:npg_3azLQtYjN0WM@ep-dry-wildflower-agwrkfeu-pooler.c-2.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
 
